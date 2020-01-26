0d42aa1423b76b76ad8d9231a2ee48e84df698ce
lbadocha <krakow@akademia108.pl>
2020-01-26 16:47:28 +0100

    Updates

---
+++ precache-manifest.c0bb3557c24782f953a43c805061a542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1af23f17794ce0eafadd770e1b41e5",
+    "url": "/btc-react/index.html"
+  },
+  {
+    "revision": "0b0eb7d21c734b95fc5e",
+    "url": "/btc-react/static/css/main.fa17b4a0.chunk.css"
+  },
+  {
+    "revision": "d53a0baae8b3db97334a",
+    "url": "/btc-react/static/js/2.a8f63be2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/btc-react/static/js/2.a8f63be2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b0eb7d21c734b95fc5e",
+    "url": "/btc-react/static/js/main.0063a014.chunk.js"
+  },
+  {
+    "revision": "f5e4f9fe53aeba080c7b",
+    "url": "/btc-react/static/js/runtime-main.adf494f8.js"
+  }
+]);
